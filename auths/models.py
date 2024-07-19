1697cfb40603d8495edc206b940cd6222abfa219
lee-youn <5816654@naver.com>
2024-07-19 15:21:51 +0900

    Fix : git 충돌 해결

--- auths/models.py
+++ auths/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+
+class MutsaUserManager(BaseUserManager):
+
+    use_in_migrations = True
+
+    def create_user(self, nickname,password=None):
+        if not nickname:
+            raise ValueError('User must have a nickname')
+        
+        user = self.model(
+            nickname=nickname,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    def create_superuser(self, nickname, password=None):
+        user = self.create_user(
+            nickname,
+            password=password
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+class MutsaUser(AbstractBaseUser):
+    nickname = models.CharField(max_length=1024, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    school = models.CharField(max_length=1024, default = '-')
+
+
+    objects = MutsaUserManager()
+
+    USERNAME_FIELD = 'nickname'
+
+    @property
+    def is_staff(self):
+        return self.is_admin
+
+
 
 # Create your models here.
