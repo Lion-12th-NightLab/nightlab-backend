d05d0dee5e7976b4833ca19d9cdb7e242fac3ecb
fewolee <chulwoo25@naver.com>
2024-07-28 20:20:59 +0900

    ✨ Feat : Swagger urls.py 설정

--- config/urls.py
+++ config/urls.py
@@ -18,10 +18,28 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
-from django.urls import path, include
+from django.urls import path, include, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import routers, permissions
+
 from auths.views import login,token_reissue #verify
 from users.views import user,users_list
 from verify.views import SendVerification, CheckVerifycode
+
+
+router = routers.DefaultRouter()
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="NightLab API",
+        default_version='v1',
+        description="NightLab API Test",
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -34,5 +52,11 @@ urlpatterns = [
     path("users/list", users_list)
 ]
 
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += [
+        re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+        re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+        re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+        ]
