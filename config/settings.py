752dbd9cc9280a7365c9a55d49aeff84fb924a85
fewolee <chulwoo25@naver.com>
2024-08-05 19:02:10 +0900

    🐛 Fix : setting.py 오타 수정

--- config/settings.py
+++ config/settings.py
@@ -201,7 +201,7 @@ SWAGGER_SETTINGS = {
         'BearerAuth': []
     }]
 }
-g
+
 SECURE_SSL_REDIRECT = True  # 모든 HTTP 요청을 HTTPS로 리다이렉트
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')  # 프록시 서버를 사용하는 경우
 
